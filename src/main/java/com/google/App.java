40a9852fd6f076e9e4d515b44fa513f7e755990e
Preran-48 <prerankumar.n@qspiders.in>
2025-06-30 19:26:15 +0530

    Update App.java

--- src/main/java/com/google/App.java
+++ src/main/java/com/google/App.java
@@ -5,7 +5,7 @@ package com.google;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!, How are you");
+        System.out.println("Hello World!");
         
         
     }
