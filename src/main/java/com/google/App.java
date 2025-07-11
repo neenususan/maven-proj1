a0291da0c027e1da10a44ac60e18d7258f8b8fd3
neenususan <neenususan60@gmail.com>
2025-07-11 23:33:43 +0530

    Update App.java

--- src/main/java/com/google/App.java
+++ src/main/java/com/google/App.java
@@ -6,7 +6,7 @@ package com.google;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!, Jenkins");
-        
-        
+        System.out.println("Hi all");
+        System.out.println("Welcome to AWS");
     }
 }
